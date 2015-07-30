12c7edd9b3008fa49f28c6eb8ce9b2879f255e36
Julian <julian.alonso.dev@gmail.com>
2015-07-30 14:29:58 +0200

    Added the views

---
+++ JAXibView.h
@@ -0,0 +1,13 @@
+//
+//  JAXibView.h
+//  KinoApp
+//
+//  Created by Julian Alonso on 30/7/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAXibView : UIView
+
+@end
---
+++ JAXibView.m
@@ -0,0 +1,91 @@
+//
+//  JAXibView.m
+//  KinoApp
+//
+//  Created by Julian Alonso on 30/7/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "JAXibView.h"
+
+@interface JAXibView ()
+
+@property (nonatomic, strong) UIView *constainerView;
+@property (nonatomic, strong) NSMutableArray *customConstraints;
+
+@end
+
+@implementation JAXibView
+
+#pragma mark - Init methods.
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self)
+    {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if (self)
+    {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (void)commonInit
+{
+    _customConstraints = [NSMutableArray new];
+    UIView *view = nil;
+    NSArray *objects = [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil];
+    
+    for (id object in objects)
+    {
+        if ([object isKindOfClass:[UIView class]])
+        {
+            view = object;
+            break;
+        }
+    }
+    
+    if (view)
+    {
+        _constainerView = view;
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:view];
+        [self setNeedsUpdateConstraints];
+    }
+    
+}
+
+#pragma mark - Update methods.
+- (void)updateConstraints
+{
+    [self removeConstraints:self.customConstraints];
+    [self.customConstraints removeAllObjects];
+    
+    if (self.constainerView)
+    {
+        UIView *view = self.constainerView;
+        
+        NSDictionary *views = NSDictionaryOfVariableBindings(view);
+        
+        [self.customConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]|" options:0 metrics:nil views:views]];
+        [self.customConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[view]|" options:0 metrics:nil views:views]];
+        
+        [self addConstraints:self.customConstraints];
+    }
+    
+    [super updateConstraints];
+}
+
+@end
